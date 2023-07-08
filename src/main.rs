53bbd88c807bdd7fbc2660771b57c9499743ba73
S4ndf1re <jan.kleinmann2000@gmail.com>
2023-07-08 18:32:52 +0200

    documenting comments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 pub mod types;
 use std::sync::Arc;
-use tracing::Level;
 use tracing::log::info;
+use tracing::Level;
 pub use types::*;
 
 pub mod business;
@@ -20,7 +20,9 @@ use axum::Router;
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt().with_max_level(Level::DEBUG).init();
+    tracing_subscriber::fmt()
+        .with_max_level(Level::DEBUG)
+        .init();
     info!("Starting up");
     let state = BusinessRules::new().await;
 
@@ -29,27 +31,32 @@ async fn main() {
 
     // build our application with a single route
     let app = Router::new()
-        .route("/item/search/:name", get(find_items))
-        .route("/item", post(add_item))
-        .route("/item", get(get_all_items))
-        .route("/item/:id", get(get_item))
-        .route("/item/:id", delete(delete_item));
+        .route("/item/search/:name", get(find_items)) // search for items by name (this can
+        // containt any query string and will even
+        // handle some fuzziness)
+        .route("/item", post(add_item)) // create a new item
+        .route("/item", get(get_all_items)) // gel all items
+        .route("/item/:id", get(get_item)) // get a specific item
+        .route("/item/:id", delete(delete_item)); // delete an item
 
     let app = app
-        .route("/category", post(new_category))
-        .route("/category", get(get_all_categories));
+        .route("/category", post(new_category)) // create a new category
+        .route("/category", get(get_all_categories)); // get all categories
 
     let app = app
-        .route("/collection", post(new_collection))
+        .route("/collection", post(new_collection)) // create a new collection
         .route(
+            // add an item to a collection
             "/collection/:collection_id/:item_id",
             post(add_item_to_collection),
         )
         .route(
+            // get all items in a collection
             "/collection/:collection_id/items",
             get(get_items_in_collection),
         )
         .route(
+            // delete an item from a collection
             "/collection/:collection_id/:item_id",
             delete(remove_item_from_collection),
         );
