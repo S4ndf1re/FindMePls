57fab695f33389770e7681dad6ec2c5d6de4164d
S4ndf1re <jan.kleinmann2000@gmail.com>
2023-08-13 16:51:05 +0200

    added item to bytes serialization

--- src/types.rs
+++ src/types.rs
@@ -73,22 +73,59 @@ impl Storeable for Item {
     fn as_bytes<'a>(&'a self) -> Result<Cow<'a, Vec<u8>>> {
         let mut data = vec![];
 
-
-        let thumbnail_data = match &self.thumbnail {
-            Some(thumbnail) => {
-                base64::engine::general_purpose::STANDARD.decode(thumbnail)?
-            }
+        let mut thumbnail_data = match &self.thumbnail {
+            Some(thumbnail) => base64::engine::general_purpose::STANDARD.decode(thumbnail)?,
             None => vec![],
         };
 
-        // data.con thumbnail_data.len().to_le_bytes();
+        data.extend_from_slice(&thumbnail_data.len().to_le_bytes());
+        data.append(&mut thumbnail_data);
+
+        let mut image_data = match &self.fullsize {
+            Some(fullsize) => base64::engine::general_purpose::STANDARD.decode(fullsize)?,
+            None => vec![],
+        };
+
+        data.extend_from_slice(&image_data.len().to_le_bytes());
+        data.append(&mut image_data);
 
         Ok(Cow::Owned(data))
     }
 
     fn change_from_bytes(&mut self, bytes: &[u8]) {
-        let thumbnail = base64::engine::general_purpose::STANDARD.encode(bytes);
+
+        // read first 4 to 8 (depending on 64 or 32 system) as the size for the thumbnail data
+        // stream
+        let mut size_bytes: [u8; (usize::BITS / 8) as usize] = [0; (usize::BITS / 8) as usize];
+        size_bytes.copy_from_slice(&bytes[0..(usize::BITS / 8) as usize]);
+        let size = usize::from_le_bytes(size_bytes);
+
+        // remove first size bytes as they are only used for the rest size
+        let rest = &bytes[((usize::BITS / 8) as usize)..];
+
+        // read thumbnail data
+        let data = &rest[0..size];
+        let rest = &rest[size..];
+
+        let thumbnail = base64::engine::general_purpose::STANDARD.encode(data);
         self.thumbnail = Some(thumbnail);
+
+        // read image size
+        let mut size_bytes: [u8; (usize::BITS / 8) as usize] = [0; (usize::BITS / 8) as usize];
+        size_bytes.copy_from_slice(&rest[0..(usize::BITS / 8) as usize]);
+        let size = usize::from_le_bytes(size_bytes);
+
+        // define rest without image size
+        let rest = &rest[((usize::BITS / 8) as usize)..];
+
+        // read image data
+        let data = &rest[0..size];
+        let rest = &rest[size..];
+
+        let image = base64::engine::general_purpose::STANDARD.encode(data);
+        self.fullsize = Some(image);
+
+        assert!(rest.len() == 0);
     }
 
     fn filename<'a>(&'a self) -> Result<Cow<'a, str>> {
@@ -99,6 +136,37 @@ impl Storeable for Item {
                 StatusCode::INTERNAL_SERVER_ERROR,
             )),
         }
+    }
+}
+
+
+#[cfg(test)]
+mod test_image_to_file {
+    use crate::{Item, Storeable};
+
+    #[test]
+    fn serialize_and_deserialize() {
+        let item = Item {
+            id : None,
+            name : "".to_owned(),
+            description: None,
+            category_id: None,
+            price: None,
+            thumbnail: Some("YXNkZg==".to_owned()),
+            fullsize: Some("ZmRhcw==".to_owned()),
+        };
+        let data = item.as_bytes(); 
+        assert!(data.is_ok());
+
+        let data = data.unwrap();
+        let mut item2 = item.clone();
+        item2.thumbnail = None;
+        item2.fullsize = None;
+
+        item2.change_from_bytes(data.as_ref());
+        assert!(item.thumbnail == item2.thumbnail);
+        assert!(item.fullsize == item2.fullsize);
+        
     }
 }
 
